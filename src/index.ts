8f81f5ee1d909dff00e5a3ebae798a6a8dd18eb6
swoocn <swooshinn@gmail.com>
2024-06-15 11:10:29 -0400

    Vercel backend hosting troubleshoot attempt; consolidate entry point.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
 import { connectDB } from "./config/dbConnection";
-import app from "./utils/app";
+import docRouter from "./docs/swagger";
+import requestLogger from "./middlewares/logger";
+import appRouter from "./routes";
+import homeRoutes from "./routes/home.routes";
+import userRoutes from "./routes/user.routes";
+import sellerRoutes from "./routes/seller.routes";
 import { env } from "./utils/env";
 
+const app = express();
+
+// Middleware setup
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(requestLogger);
+
+app.use(cors({
+    origin: "*"
+}));
+
+// Routes setup
+app.use("/api/v1", appRouter);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/sellers", sellerRoutes);
+app.use("/", homeRoutes);
+app.use("/", docRouter);
+
+// DB setup
 app.listen(env.PORT, async () => {
   try {
     await connectDB();
     console.log(`Server is running on port ${env.PORT}`);
-  } catch (error:any) {
+  } catch (error: any) {
     console.log("Server failed to run", error.message);
   }
 });
