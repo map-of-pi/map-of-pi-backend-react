bc172feaf83a344b3944a29c92670d9ec3a3f1d7
swoocn <swooshinn@gmail.com>
2024-07-09 23:31:44 -0400

    Misc adjustment to Swagger setup.

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors"
 import path from "path";
 
-import docRouter from "../docs/swagger";
+import docRouter from "../config/swagger";
 import requestLogger from "../middlewares/logger";
 
 import appRouter from "../routes";
