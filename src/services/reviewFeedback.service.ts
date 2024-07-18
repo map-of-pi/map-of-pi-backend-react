f338d2c885b88e8dc61a079f1a9226a52d05a573
adisa39 <116845543+adisa39@users.noreply.github.com>
2024-07-18 08:22:08 +0100

    added function to resolve reviewer_giver_id to reviewer username to reviewFeedback.service

--- src/services/reviewFeedback.service.ts
+++ src/services/reviewFeedback.service.ts
@@ -1,20 +1,46 @@
 import ReviewFeedback from "../models/ReviewFeedback";
+import User from "../models/User";
 import { IReviewFeedback } from "../types";
+
+
+const resolveUsername = async (reviewer_id: string): Promise<string> => {
+  const reviewer = await User.findOne({ uid: reviewer_id }).exec();
+  return reviewer ? reviewer.username : '';
+};
 
 export const getReviewFeedback = async (review_receiver_id: string): Promise<IReviewFeedback[]> => {
   try {
-    const reviewFeedback = await ReviewFeedback.find({review_receiver_id}).exec();
-    return reviewFeedback;
+    const reviewFeedbackList = await ReviewFeedback.find({ review_receiver_id }).exec();
+
+    // Update each reviewFeedback item with the reviewer's username instead of ID
+    const updatedReviewFeedbackList = await Promise.all(
+      reviewFeedbackList.map(async (reviewFeedback) => {
+        const username = await resolveUsername(reviewFeedback.review_giver_id);
+        return { ...reviewFeedback.toObject(), review_giver_id: username };
+      })
+    );
+
+    return updatedReviewFeedbackList as IReviewFeedback[] ;
   } catch (error: any) {
     console.error(`Error retrieving review feedback collection for userID ${review_receiver_id}: `, error.message);
     throw new Error(error.message);
   }
 };
 
+
 export const getReviewFeedbackById = async (review_id: string): Promise<IReviewFeedback | null> => {
   try {
-    const reviewFeedback = await ReviewFeedback.findOne({review_id}).exec();
-    return reviewFeedback;
+    const reviewFeedback = await ReviewFeedback.findOne({ review_id }).exec();
+
+    if (!reviewFeedback) {
+      return null; // Return early if review feedback is not found
+    }
+
+    // Update reviewFeedback with reviewer's username instead of ID
+    const username = await resolveUsername(reviewFeedback.review_giver_id);
+    reviewFeedback.review_giver_id = username;
+
+    return reviewFeedback as IReviewFeedback; // Return the modified review feedback object
   } catch (error: any) {
     console.error(`Error retrieving review feedback with reviewID ${review_id}:`, error.message);
     throw new Error(error.message);
