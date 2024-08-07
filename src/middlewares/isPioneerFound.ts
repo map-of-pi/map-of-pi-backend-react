a1458726bb7b3c1882714e5e9b2fa3d63588f289
swoocn <swooshinn@gmail.com>
2024-08-06 21:56:28 -0400

    PR adjustement to maintain is*Found middleware consistency.

--- src/middlewares/isPioneerFound.ts
+++ src/middlewares/isPioneerFound.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+
 import { platformAPIClient } from "../config/platformAPIclient";
-import { IUser } from "../types";
-import User from "../models/User";
 
 export const isPioneerFound = async (
     req: Request,
@@ -13,27 +12,22 @@ export const isPioneerFound = async (
     try {
       // Verify the user's access token with the /me endpoint:
       const me = await platformAPIClient.get(`/v2/me`, { headers: { 'Authorization': `Bearer ${auth.accessToken}` } });
-      console.log('Pioneer data obtained from Pioneer authentication', me.data);
       
-      if(!me){
-        return res.status(401).json({
-            message: "Pioneer user not authorized",
-          });
+      if (me) {
+        const user = {
+          pi_uid: me.data.uid,
+          pi_username: me.data.username,
+          user_name: me.data.username
+        }
+        req.body.user = user;
+        return next();
+      } else {
+        return res.status(404).json({
+          message: "Pioneer not found",
+        });
       }
-
-      const user = {
-        pi_uid: me.data.uid,
-        pi_username: me.data.username,
-        user_name: me.data.username
-      }
-
-      req.body.user = user;
-      return next();
-
-    } catch (err) {
-      console.log(err);
-      return res.status(401).json({error: "Invalid access token"}) 
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
     }
-    
   };
   
