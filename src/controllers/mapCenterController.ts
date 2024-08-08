9e5ac06e96bfc9aec870193d6d20563a48621f34
Darin Hajou <darinhajou@gmail.com>
2024-08-08 12:20:18 +0200

    Simplify mapCenter controller by moving data logic to service

--- src/controllers/mapCenterController.ts
+++ src/controllers/mapCenterController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import MapCenter from '../models/MapCenter';
+import * as MapCenterService from '../services/mapCenter.service'; 
 
 const isError = (error: unknown): error is Error => {
   return error instanceof Error;
@@ -8,22 +9,15 @@ const isError = (error: unknown): error is Error => {
 export const saveMapCenter = async (req: Request, res: Response) => {
   const { pi_uid, latitude, longitude } = req.body;
   try {
-    if (!pi_uid || latitude === undefined || longitude === undefined) {
-      return res.status(400).json({ message: 'Missing required fields' });
-    }
-    let mapCenter = await MapCenter.findOneAndUpdate(
-      { pi_uid },
-      { latitude, longitude },
-      { new: true, upsert: true }
-    );
+    const mapCenter = await MapCenterService.createOrUpdateMapCenter(pi_uid, latitude, longitude);
     res.status(200).json({ message: 'Map center saved successfully', mapCenter });
-  } catch (error) {
-    if (isError(error)) {
-      console.error('Error saving map center:', error.message);
-      res.status(500).json({ message: 'Error saving map center', error: error.message });
-    } else {
-      console.error('Unknown error occurred while saving map center');
-      res.status(500).json({ message: 'Unknown error occurred while saving map center' });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+    console.error('Error saving map center:', error.message);
+    res.status(500).json({ message: 'Error saving map center', error: error.message });
+  } else {
+    console.error('Unknown error occurred while saving map center');
+    res.status(500).json({ message: 'Unknown error occurred while saving map center'});
     }
   }
 };
