c8a99416e2ac28d268f39ee7e3ecf55ee28fcfc7
Darin Hajou <darinhajou@gmail.com>
2024-08-08 12:57:25 +0200

    Refactor mapCenter.routes to use PUT for updates and add verifyToken for security

--- src/routes/mapCenter.routes.ts
+++ src/routes/mapCenter.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { saveMapCenter, getMapCenter } from '../controllers/mapCenterController';
+import { verifyToken } from '../middlewares/verifyToken';
 
 const mapCenterRoutes = Router();
 
 // Route to get the map center for a specific user
-mapCenterRoutes.get('/:pi_uid', getMapCenter);
+mapCenterRoutes.get('/:pi_uid', verifyToken, getMapCenter);
 
 // Route to save or update the map center
-mapCenterRoutes.post('/', saveMapCenter);
+mapCenterRoutes.put('/save', verifyToken, saveMapCenter);
 
 export default mapCenterRoutes;
