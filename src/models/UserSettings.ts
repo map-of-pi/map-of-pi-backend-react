873de0e35316984d0902ff3d28d7560472aeb820
Darin Hajou <darinhajou@gmail.com>
2024-10-01 14:55:18 +0200

    Set default values to null in userSettings.ts

--- src/models/UserSettings.ts
+++ src/models/UserSettings.ts
@@ -1,61 +1,63 @@
-import mongoose, { Schema } from "mongoose";
+  import mongoose, { Schema } from "mongoose";
 
-import { IUserSettings } from "../types";
-import { TrustMeterScale } from "./enums/trustMeterScale";
+  import { IUserSettings } from "../types";
+  import { TrustMeterScale } from "./enums/trustMeterScale";
 
-const userSettingsSchema = new Schema<IUserSettings>(
-  {
-    user_settings_id: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    user_name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: false,
-    },
-    phone_number: {
-      type: String,
-      required: false,
-    },
-    image: {
-      type: String,
-      required: false,
-    },
-    findme: {
-      type: String,
-      required: true,
-      default: 'deviceGPS'
-    },
-    trust_meter_rating: {
-      type: Number,
-      enum: Object.values(TrustMeterScale).filter(value => typeof value === 'number'),
-      required: true,
-      default: TrustMeterScale.HUNDRED
-    },
-    search_map_center: {
-      type: {
+  const userSettingsSchema = new Schema<IUserSettings>(
+    {
+      user_settings_id: {
         type: String,
-        enum: ['Point'],
-        required: false,
-        default: 'Point',
+        required: true,
+        unique: true
       },
-      coordinates: {
-        type: [Number],
-        required: false,
-        default: [0, 0]
+      user_name: {
+        type: String,
+        required: true,
       },
+      email: {
+        type: String,
+        required: false,
+        default: null,
+      },
+      phone_number: {
+        type: String,
+        required: false,
+        default: null,
+      },
+      image: {
+        type: String,
+        required: false,
+      },
+      findme: {
+        type: String,
+        required: true,
+        default: 'deviceGPS'
+      },
+      trust_meter_rating: {
+        type: Number,
+        enum: Object.values(TrustMeterScale).filter(value => typeof value === 'number'),
+        required: true,
+        default: TrustMeterScale.HUNDRED
+      },
+      search_map_center: {
+        type: {
+          type: String,
+          enum: ['Point'],
+          required: false,
+          default: 'Point',
+        },
+        coordinates: {
+          type: [Number],
+          required: false,
+          default: [0, 0]
+        },
+      }
     }
-  }
-);
+  );
 
-// use GeoJSON format to store geographical data i.e., points using '2dsphere' index.
-userSettingsSchema.index({ search_map_center: '2dsphere' });
+  // use GeoJSON format to store geographical data i.e., points using '2dsphere' index.
+  userSettingsSchema.index({ search_map_center: '2dsphere' });
 
-const UserSettings = mongoose.model<IUserSettings>("User-Settings", userSettingsSchema);
+  const UserSettings = mongoose.model<IUserSettings>("User-Settings", userSettingsSchema);
 
-export default UserSettings;
+  export default UserSettings;
