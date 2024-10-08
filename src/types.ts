85733ade59af402bccaeb9dcc679763cd4924aed
Darin Hajou <darinhajou@gmail.com>
2024-10-08 03:37:14 +0200

    Allow email and phone_number to be null in types

--- src/types.ts
+++ src/types.ts
@@ -1,69 +1,69 @@
-import { Document, Types } from "mongoose";
-import { DeviceLocationType } from "./models/enums/deviceLocationType";
-import { RatingScale } from "./models/enums/ratingScale";
-import { SellerType } from "./models/enums/sellerType";
-import { TrustMeterScale } from "./models/enums/trustMeterScale";
+  import { Document, Types } from "mongoose";
+  import { DeviceLocationType } from "./models/enums/deviceLocationType";
+  import { RatingScale } from "./models/enums/ratingScale";
+  import { SellerType } from "./models/enums/sellerType";
+  import { TrustMeterScale } from "./models/enums/trustMeterScale";
 
-export interface IUser extends Document {
-  pi_uid: string;
-  pi_username: string;
-  user_name: string;
-}
+  export interface IUser extends Document {
+    pi_uid: string;
+    pi_username: string;
+    user_name: string;
+  }
 
-export interface IUserSettings extends Document {
-  user_settings_id: string;
-  user_name: string;
-  email?: string;
-  phone_number?: string;
-  image?: string; 
-  findme: DeviceLocationType;
-  trust_meter_rating: TrustMeterScale;
-  search_map_center?: {
+  export interface IUserSettings extends Document {
+    user_settings_id: string;
+    user_name: string;
+    email?: string | null;
+    phone_number?: string | null;
+    image?: string; 
+    findme: DeviceLocationType;
+    trust_meter_rating: TrustMeterScale;
+    search_map_center?: {
+      type: 'Point';
+      coordinates: [number, number];
+    };
+  }
+
+  export interface ISeller extends Document {
+    seller_id: string;
+    name: string;
+    seller_type: SellerType;
+    description: string;
+    image?: string;
+    address?: string;
+    average_rating: Types.Decimal128;
+    sell_map_center: {
+      type: 'Point';
+      coordinates: [number, number];
+    };
+    order_online_enabled_pref: boolean;
+  }
+
+  export interface IReviewFeedback extends Document {
+    _id: string;
+    review_receiver_id: string;
+    review_giver_id: string;
+    reply_to_review_id: string | null;
+    rating: RatingScale;
+    comment?: string;
+    image?: string;
+    review_date: Date;
+  }
+
+  export interface IMapCenter {
     type: 'Point';
     coordinates: [number, number];
-  };
-}
+  }
 
-export interface ISeller extends Document {
-  seller_id: string;
-  name: string;
-  seller_type: SellerType;
-  description: string;
-  image?: string;
-  address?: string;
-  average_rating: Types.Decimal128;
-  sell_map_center: {
-    type: 'Point';
-    coordinates: [number, number];
-  };
-  order_online_enabled_pref: boolean;
-}
+  // Select specific fields from IUserSettings
+  export type PartialUserSettings = Pick<IUserSettings, 'user_name' | 'email' | 'phone_number' | 'findme' | 'trust_meter_rating'>;
 
-export interface IReviewFeedback extends Document {
-  _id: string;
-  review_receiver_id: string;
-  review_giver_id: string;
-  reply_to_review_id: string | null;
-  rating: RatingScale;
-  comment?: string;
-  image?: string;
-  review_date: Date;
-}
+  // Combined interface representing a seller with selected user settings
+  export interface ISellerWithSettings extends ISeller, PartialUserSettings {}
 
-export interface IMapCenter {
-  type: 'Point';
-  coordinates: [number, number];
-}
+  export type PartialReview = {
+    giver: string;
+    receiver: string;
+  }
 
-// Select specific fields from IUserSettings
-export type PartialUserSettings = Pick<IUserSettings, 'user_name' | 'email' | 'phone_number' | 'findme' | 'trust_meter_rating'>;
-
-// Combined interface representing a seller with selected user settings
-export interface ISellerWithSettings extends ISeller, PartialUserSettings {}
-
-export type PartialReview = {
-  giver: string;
-  receiver: string;
-}
-
-export interface IReviewFeedbackOutput extends IReviewFeedback, PartialReview {}
+  export interface IReviewFeedbackOutput extends IReviewFeedback, PartialReview {}
