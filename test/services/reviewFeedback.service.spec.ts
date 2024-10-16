e97229220d51ce4c885bab5b94f3022b5bf53530
swoocn <swooshinn@gmail.com>
2024-10-15 20:44:43 -0400

    Mod reviewFeedback unit test in service layer to use MMS.

--- test/services/reviewFeedback.service.spec.ts
+++ test/services/reviewFeedback.service.spec.ts
@@ -1,132 +1,52 @@
-import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
-
-import { getReviewFeedbackById, addReviewFeedback } from '../../src/services/reviewFeedback.service';
+import { addReviewFeedback, getReviewFeedbackById } from '../../src/services/reviewFeedback.service';
 import ReviewFeedback from '../../src/models/ReviewFeedback';
 import User from '../../src/models/User';
-import * as userService from '../../src/services/user.service';
-import { IUser } from '../../src/types';
-
-let mongoServer: MongoMemoryServer;
-
-const mockUsers = [
-  {
-    pi_uid: '0a0a0a-0a0a-0a0a',
-    pi_username: 'TestUser_1',
-    user_name: 'test-user-1'
-  },
-  {
-    pi_uid: '0b0b0b-0b0b-0b0b',
-    pi_username: 'TestUser_2',
-    user_name: 'test-user-2'
-  },
-  {
-    pi_uid: '0c0c0c-0c0c-0c0c',
-    pi_username: 'TestUser_3',
-    user_name: 'test-user-3'
-  },
-  {
-    pi_uid: '0d0d0d-0d0d-0d0d',
-    pi_username: 'TestUser_4',
-    user_name: 'test-user-4'
-  },
-] as IUser[];
-
-const mockReview = {
-  _id: new mongoose.Types.ObjectId('64f5a0f2a86d1f9f3b7e4e81'),
-  review_receiver_id: '0a0a0a-0a0a-0a0a',
-  review_giver_id: '0b0b0b-0b0b-0b0b',
-  comment: '0b0b0b-0b0b-0b0b Test Review Comment',
-  rating: 5,
-  review_date: new Date()
-};
-
-const mockReviewReplies = [
-  {
-    _id: new mongoose.Types.ObjectId('64f5a0f2a86d1f9f3b7e4e82'),
-    review_receiver_id: '0a0a0a-0a0a-0a0a',
-    review_giver_id: '0c0c0c-0c0c-0c0c',
-    reply_to_review_id: '64f5a0f2a86d1f9f3b7e4e81',
-    comment: '0c0c0c-0c0c-0c0c Test Reply Comment',
-    rating: 4,
-    review_date: new Date()
-  },
-  {
-    _id: new mongoose.Types.ObjectId('64f5a0f2a86d1f9f3b7e4e83'),
-    review_receiver_id: '0a0a0a-0a0a-0a0a',
-    review_giver_id: '0d0d0d-0d0d-0d0d',
-    reply_to_review_id: '64f5a0f2a86d1f9f3b7e4e81',
-    comment: '0d0d0d-0d0d-0d0d Test Reply Comment',
-    rating: 3,
-    review_date: new Date()
-  },
-  {
-    _id: new mongoose.Types.ObjectId('64f5a0f2a86d1f9f3b7e4e84'),
-    review_receiver_id: '0b0b0b-0b0b-0b0b',
-    review_giver_id: '0e0e0e-0e0e-0e0e',
-    reply_to_review_id: null,
-    comment: '0e0e0e-0e0e-0e0e Test Reply Comment',
-    rating: 5,
-    review_date: new Date()
-  }
-];
-
-beforeAll(async () => {
-  try {
-    mongoServer = await MongoMemoryServer.create();
-    const uri = mongoServer.getUri();
-    await mongoose.connect(uri, { dbName: 'test' });
-
-    // initialize in-memory MongoDB by inserting mock data records
-    await ReviewFeedback.insertMany([mockReview, ...mockReviewReplies]);
-    await User.insertMany(mockUsers);
-  } catch (error) {
-    console.error('Failed to start MongoMemoryServer', error);
-    throw error;
-  }
-});
-
-afterAll(async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
-});
+import { IReviewFeedback, IUser } from '../../src/types';
 
 describe('getReviewFeedbackById function', () => {
-  // Mock ReviewFeedback.findById for fetching the main review
-  jest.spyOn(ReviewFeedback, 'findById').mockResolvedValue(mockReview);
-  // Mock ReviewFeedback.find for fetching the replies
-
-  jest.spyOn(ReviewFeedback, 'find').mockResolvedValue(mockReviewReplies);
-  // Mock the getUser function to return the mock user records
-  jest.spyOn(userService, 'getUser').mockResolvedValue(mockUsers[0]);
-
   it('should return the main review and its associated replies', async () => {
-    const result = await getReviewFeedbackById(mockReview._id.toString());
+    const usersData = await User.find();
+
+    const reviewData = await ReviewFeedback.findOne({
+      _id: '64f5a0f2a86d1f9f3b7e4e81' 
+    }) as IReviewFeedback;
+
+    const reviewRepliesData = await ReviewFeedback.find({
+      reply_to_review_id: reviewData._id
+    }) as IReviewFeedback[];
+
+    const result = await getReviewFeedbackById(reviewData._id.toString());
+
+    // Convert Mongoose documents to plain objects
+    const plainReviewData = reviewData.toObject();
+    const plainReviewRepliesData = reviewRepliesData
+      .map(reply => reply.toObject())
+      .sort((a, b) => new Date(b.review_date).getTime() - new Date(a.review_date).getTime());
 
     // Assertions for the main review
     expect(result?.review).toMatchObject({
-      ...mockReview,
-      giver: mockUsers[1].user_name,
-      receiver: mockUsers[0].user_name,
+      ...plainReviewData,
+      giver: usersData[1].user_name,
+      receiver: usersData[0].user_name,
       comment: '0b0b0b-0b0b-0b0b Test Review Comment',
       rating: 5
     });
 
-    // Assertions for the replies
+    // Assertions for the replies sorted by most recent review_date.
     expect(result?.replies).toHaveLength(2);
     expect(result?.replies[0]).toMatchObject({
-      ...mockReviewReplies[0],
-      giver: mockUsers[2].user_name,
-      receiver: mockUsers[0].user_name,
-      comment: '0c0c0c-0c0c-0c0c Test Reply Comment',
-      rating: 4,
-    });
-    expect(result?.replies[1]).toMatchObject({
-      ...mockReviewReplies[1],
-      giver: mockUsers[3].user_name,
-      receiver: mockUsers[0].user_name,
+      ...plainReviewRepliesData[0],
+      giver: usersData[3].user_name,
+      receiver: usersData[0].user_name,
       comment: '0d0d0d-0d0d-0d0d Test Reply Comment',
       rating: 3,
+    });
+    expect(result?.replies[1]).toMatchObject({
+      ...plainReviewRepliesData[1],
+      giver: usersData[2].user_name,
+      receiver: usersData[0].user_name,
+      comment: '0c0c0c-0c0c-0c0c Test Reply Comment',
+      rating: 4,
     });
   });
 });
@@ -136,27 +56,25 @@ describe('addReviewFeedback function', () => {
     review_receiver_id: '0a0a0a-0a0a-0a0a',
     review_giver_id: '0b0b0b-0b0b-0b0b',
     reply_to_review_id: null,
-    rating: '5',
+    rating: 5,
     comment: '0b0b0b-0b0b-0b0b Test Review Comment',
     image: 'http://example.com/image_new.jpg'
   };
 
   it('should add new review feedback and compute ratings', async () => {
-    // mock the save function to return the newly created review
-    const mockSave = jest.fn().mockResolvedValue({
-      ...formData
-    })
-    jest.spyOn(ReviewFeedback.prototype, 'save').mockImplementation(mockSave);
+    const userData = await User.findOne({
+      pi_uid: '0b0b0b-0b0b-0b0b'
+    }) as IUser;
 
-    // TODO - mock computeRatings function
-
-    const result = await addReviewFeedback(mockUsers[0], formData, formData.image);
+    const reviewFeedbackData = await addReviewFeedback(userData, formData, formData.image);
     
-    expect(result).toHaveProperty('review_receiver_id', formData.review_receiver_id);
-    expect(result).toHaveProperty('review_giver_id', formData.review_giver_id);
-    expect(result).toHaveProperty('reply_to_review_id', formData.reply_to_review_id);
-    expect(result).toHaveProperty('rating', formData.rating);
-    expect(result).toHaveProperty('comment', formData.comment);
-    expect(result).toHaveProperty('image', formData.image);
+    expect(reviewFeedbackData).toEqual(expect.objectContaining({
+      review_receiver_id: formData.review_receiver_id,
+      review_giver_id: formData.review_giver_id,
+      reply_to_review_id: formData.reply_to_review_id,
+      rating: formData.rating,
+      comment: formData.comment,
+      image: formData.image
+    }));
   });
 });
