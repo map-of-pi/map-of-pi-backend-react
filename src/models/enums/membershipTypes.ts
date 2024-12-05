d5926cd1861801923f8627b7e19e1ca518cc5ab5
Darin Hajou <darinhajou@gmail.com>
2024-12-06 00:25:21 +0100

    Define the MembershipType enum

---
+++ src/models/enums/membershipTypes.ts
@@ -0,0 +1,8 @@
+export enum MembershipType {
+    CASUAL = "Casual",
+    MEMBER = "Member",
+    GREEN = "Green",
+    GOLD = "Gold",
+    DOUBLE_GOLD = "Double Gold",
+    TRIPLE_GOLD = "Triple Gold",
+  }  
