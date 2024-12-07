37ba2957b925442ba636d96a90644d1892195c13
swoocn <swooshinn@gmail.com>
2024-12-06 23:47:22 -0500

    Added admin credentials middleware + unit tests.

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -49,3 +49,29 @@ export const verifyToken = async (
     return res.status(500).json({ message: 'Failed to verify token; please try again later' });
   }
 };
+
+export const verifyAdminToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { ADMIN_API_USERNAME, ADMIN_API_PASSWORD } = process.env;
+
+  const authHeader = req.headers.authorization;
+  const base64Credentials = authHeader && authHeader.split(" ")[1];
+  if (!base64Credentials) {
+    logger.warn("Admin credentials are missing.");
+    return res.status(401).json({ message: "Admin credentials are required" });
+  }
+
+  const credentials = Buffer.from(base64Credentials, "base64").toString("ascii");
+  const [username, password] = credentials.split(":");
+
+  if (username !== ADMIN_API_USERNAME || password !== ADMIN_API_PASSWORD) {
+    logger.warn("Admin credentials are invalid.");
+    return res.status(401).json({ message: "Admin credentials are invalid" });
+  }
+
+  logger.info("Admin credentials verified successfully.");
+  next();
+};
