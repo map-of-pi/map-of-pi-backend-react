ea2717ad8baf50b1667cbdfe2c5b4baaa4234d8c
Darin Hajou <darinhajou@gmail.com>
2025-01-13 16:19:00 +0100

    Add Tier routes in app.ts

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -16,6 +16,7 @@ import reviewFeedbackRoutes from "../routes/reviewFeedback.routes";
 import mapCenterRoutes from "../routes/mapCenter.routes";
 import reportRoutes from "../routes/report.routes";
 import membershipRoutes from "../routes/membership.routes";
+import tierRoutes from "../routes/tiers.routes";
 
 dotenv.config();
 
@@ -48,6 +49,7 @@ app.use("/api/v1/review-feedback", reviewFeedbackRoutes);
 app.use("/api/v1/map-center", mapCenterRoutes);
 app.use("/api/v1/reports", reportRoutes);
 app.use("/api/v1/membership", membershipRoutes);
+app.use("/api/v1", tierRoutes);
 
 
 app.use("/", homeRoutes);
