aa47cb37ed2d6abdc62bf27b9a9821a9af0d1b5c
Darin Hajou <darinhajou@gmail.com>
2025-01-16 15:51:54 +0100

    Recreate Membership.ts

---
+++ src/models/enums/membershipType.ts
@@ -0,0 +1,9 @@
+export enum MembershipType {
+    CASUAL = "Casual",
+    MEMBER = "Member",
+    GREEN = "Green",
+    GOLD = "Gold",
+    DOUBLE_GOLD = "Double Gold",
+    TRIPLE_GOLD = "Triple Gold",
+  }
+  
