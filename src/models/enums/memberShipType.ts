cabf7d493969578d951f8daa44c7069befa4b8e0
Darin Hajou <darinhajou@gmail.com>
2025-01-16 15:30:28 +0100

    Remove incorrectly cased memberShipType file

--- src/models/enums/memberShipType.ts
+++
@@ -1,8 +0,0 @@
-export enum MembershipType {
-    CASUAL = "Casual",
-    MEMBER = "Member",
-    GREEN = "Green",
-    GOLD = "Gold",
-    DOUBLE_GOLD = "Double Gold",
-    TRIPLE_GOLD = "Triple Gold",
-}
