49bb9e33612f6fb1ec0945805b63cedd50f32fb0
Darin Hajou <darinhajou@gmail.com>
2025-04-16 01:33:08 +0200

    Replace transaction routes with new payment routes for membership flow

---
+++ src/routes/payment.routes.ts
@@ -0,0 +1,78 @@
+// src/routes/paymentRoutes.ts
+
+import express from "express";
+import { verifyToken } from "../middlewares/verifyToken";
+import * as paymentController from "../controllers/paymentController";
+
+const paymentRoutes = express.Router();
+
+/**
+ * @swagger
+ * /api/v1/payments/initiate:
+ *   post:
+ *     tags:
+ *       - Payments
+ *     summary: Initiate a new payment
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '/api/docs/PaymentsSchema.yml#/components/schemas/InitiatePaymentRq'
+ *     responses:
+ *       201:
+ *         description: Payment initiated
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Server error
+ */
+paymentRoutes.post(
+  "/initiate",
+  verifyToken,
+  paymentController.initiatePayment
+);
+
+/**
+ * @swagger
+ * /api/v1/payments/approve:
+ *   post:
+ *     tags:
+ *       - Payments
+ *     summary: Approve a pending payment
+ */
+paymentRoutes.post(
+  "/approve",
+  verifyToken,
+  paymentController.approvePayment
+);
+
+/**
+ * @swagger
+ * /api/v1/payments/complete:
+ *   post:
+ *     tags:
+ *       - Payments
+ *     summary: Complete an approved payment
+ */
+paymentRoutes.post(
+  "/complete",
+  verifyToken,
+  paymentController.completePayment
+);
+
+/**
+ * @swagger
+ * /api/v1/payments/cancel:
+ *   post:
+ *     tags:
+ *       - Payments
+ *     summary: Cancel a pending payment
+ */
+paymentRoutes.post(
+  "/cancel",
+  verifyToken,
+  paymentController.cancelPayment
+);
+
+export default paymentRoutes;
