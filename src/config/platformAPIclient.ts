1ff3262c9eb127043d383eb66c2609bb003273aa
swoocn <swooshinn@gmail.com>
2025-05-02 01:06:13 -0400

    Misc PR adjustments; mod fallback seed to comply with seed rules [revisited].

--- src/config/platformAPIclient.ts
+++ src/config/platformAPIclient.ts
@@ -14,7 +14,7 @@ const apiKey = env.PI_API_KEY || '';
 
 /* Fallback randomly generated seed for test environments to prevent build failures 
    Starts with 'S' + 56 characters + passes Stellar's base32 & checksum rules */
-const walletSeed = env.WALLET_PRIVATE_SEED || 'SB2VZP2WZJ7VJ3J27OZJ7VJ3J27OZJ7VJ3J27OZJ7VJ3J27OZJ7VVZ';
+const walletSeed = env.WALLET_PRIVATE_SEED || 'SCOMN4G6WNHWDZ3YCCYJ5NBGQG6FQOVPCCXKDPLH7VQFPLM5PY5X6Z3D';
 
 const pi = new PiNetwork(apiKey, walletSeed);
 export default pi
