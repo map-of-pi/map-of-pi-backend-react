ffa6047a0d617aacd0e6115301f5789073b9bc92
Darin Hajou <darin.hajou@gmail.com>
2025-06-19 17:28:18 +0200

    Implement final membership model (aligned with Haycoder’s schema)

---
+++ src/controllers/membership.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import { MembershipType } from "./enums/memberShipType";
+
+const membershipSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    pi_uid: {
+      type: String,
+      required: true,
+    },
+    membership_class: {
+      type: String,
+      required: true,
+      default: MembershipType.CASUAL,
+      enum: Object.values(MembershipType),
+    },
+    mappi_balance: {
+      type: Number,
+      required: true,
+      default: 0,
+      validate: {
+        validator: (value: number) => value >= 0,
+        message: "mappi_balance must be a non-negative number.",
+      },
+    },
+    membership_expiration: {
+      type: Date,
+      required: false,
+      default: null,
+    },
+    mappi_used_to_date: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+// Indexes
+membershipSchema.index({ user_id: 1 });
+membershipSchema.index({ pi_uid: 1 });
+
+export default mongoose.model('Membership', membershipSchema);
