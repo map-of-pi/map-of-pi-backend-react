a1d8fb0b9edb4c3d255909b8619b6ace03acb41d
Darin Hajou <darin.hajou@gmail.com>
2025-06-26 20:32:16 +0200

    Add tierRank helper to support membership upgrade/downgrade logic

--- src/models/enums/membershipClassType.ts
+++ src/models/enums/membershipClassType.ts
@@ -6,3 +6,13 @@ export enum MembershipClassType {
   DOUBLE_GOLD = "Double Gold",
   TRIPLE_GOLD = "Triple Gold",
 }
+
+// Used for rank-based comparisons (upgrade/downgrade/etc)
+export const tierRank: Record<MembershipClassType, number> = {
+  [MembershipClassType.CASUAL]: 0,
+  [MembershipClassType.MEMBER]: 1,
+  [MembershipClassType.GREEN]: 2,
+  [MembershipClassType.GOLD]: 3,
+  [MembershipClassType.DOUBLE_GOLD]: 4,
+  [MembershipClassType.TRIPLE_GOLD]: 5,
+};
