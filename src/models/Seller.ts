a7075e54116be236ee0bfd418331b8e799dce915
Fredrick Amoako <fredrick.amoako@amalitech.com>
2025-07-05 14:27:28 +0000

    feat: add schema for sanction regions

--- src/models/Seller.ts
+++ src/models/Seller.ts
@@ -75,6 +75,11 @@ const sellerSchema = new Schema<ISeller>(
       type: Boolean,
       default: false,
       required: false
+    },
+    isRestricted: {
+        type: Boolean,
+        default: false,
+        required: false
     }
   },
   { timestamps: true } // Adds timestamps to track creation and update times
