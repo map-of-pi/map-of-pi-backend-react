a234cc5000cb333e875e051fc8e11f52f93bde5a
swoocn <swooshinn@gmail.com>
2025-07-25 02:14:44 -0400

    Misc PR adjustments; Membership controller layer cleanup + unit tests.

--- src/controllers/membershipController.ts
+++ src/controllers/membershipController.ts
@@ -1,17 +1,15 @@
 import { Request, Response } from "express";
 import * as membershipService from "../services/membership.service";
-import logger from "../config/loggingConfig";
 import { IUser } from "../types"
+import logger from "../config/loggingConfig";
 
 export const fetchMembershipList = async (req: Request, res: Response) => {
   try {
     const membershipList = await membershipService.buildMembershipList();
-
-    if (!membershipList) {
+    if (!membershipList || membershipList.length === 0) {
       logger.warn(`No membership list found.`);
       return res.status(404).json({ message: "Membership list not found" });
     }
-
     logger.info(`Fetched membership list`);
     return res.status(200).json(membershipList);
     
@@ -23,36 +21,28 @@ export const fetchMembershipList = async (req: Request, res: Response) => {
 
 export const fetchUserMembership = async (req: Request, res: Response) => {
   const authUser = req.currentUser as IUser;
-
   try {
     const currentMembership = await membershipService.getUserMembership(authUser);
-
     if (!currentMembership) {
-      logger.warn(`Membership with ID ${authUser.pi_uid} not found.`);
-      return res.status(404).json({ message: "Membership not found" });
+      logger.warn(`User Membership with ID ${authUser.pi_uid} not found.`);
+      return res.status(404).json({ message: "User Membership not found" });
     }
-
-    logger.info(`Fetched membership with ID ${authUser.pi_uid}`);
+    logger.info(`Fetched user membership with ID ${authUser.pi_uid}`);
     return res.status(200).json(currentMembership);
   } catch (error) {
-    logger.error(`Error getting membership ID ${authUser.pi_uid}:`, error);
-    return res.status(500).json({ message: 'An error occurred while getting single membership; please try again later' });
+    logger.error(`Failed to fetch user membership with ID ${authUser.pi_uid}:`, error);
+    return res.status(500).json({ message: 'An error occurred while fetching user membership; please try again later' });
   }
 };
 
-
-// Controller to fetch a single membership by its ID 
 export const getSingleMembership = async (req: Request, res: Response) => {
   const { membership_id } = req.params;
-
   try {
     const membership = await membershipService.getSingleMembershipById(membership_id);
-
     if (!membership) {
       logger.warn(`Membership with ID ${membership_id} not found.`);
       return res.status(404).json({ message: "Membership not found" });
     }
-
     logger.info(`Fetched membership with ID ${membership_id}`);
     return res.status(200).json(membership);
   } catch (error) {
@@ -64,15 +54,16 @@ export const getSingleMembership = async (req: Request, res: Response) => {
 export const updateOrRenewMembership = async (req: Request, res: Response) => { 
   try {
     const { membership_class } = req.body;
-    const authUser = req.currentUser
-
-    if (!authUser) return res.status(401).json({ error: "Unauthorized - user not authenticated "});
-
-    const updated = await membershipService.updateOrRenewMembership(authUser.pi_uid, membership_class);
-
-    return res.status(200).json(updated);
+    const authUser = req.currentUser;
+    if (!authUser) {
+      logger.warn('No authenticated user found when updating/ renewing membership.');
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    logger.info(`Updated or renewed membership for user ${authUser.pi_uid}`);
+    const updatedMembership = await membershipService.updateOrRenewMembership(authUser.pi_uid, membership_class);
+    return res.status(200).json(updatedMembership);
   } catch (error: any) {
     logger.error("Failed to update or renew membership:", error);
-    return res.status(400).json({ error: error.message });
+    return res.status(500).json({ message: 'An error occurred while updating/ renewing membership; please try again later' });
   }
 };
