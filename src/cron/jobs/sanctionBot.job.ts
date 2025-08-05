5c5e4c277b67575d7e9d9e1b66a70ea7e536ef8b
swoocn <swooshinn@gmail.com>
2025-08-04 21:35:13 -0400

    Misc PR adjustments; mod sanctionBot job + add unit tests.

--- src/cron/jobs/sanctionBot.job.ts
+++ src/cron/jobs/sanctionBot.job.ts
@@ -1,13 +1,12 @@
+import { findAndRestrictSanctionedSellers } from "../utils/sanctionUtils";
 import logger from "../../config/loggingConfig";
-import {findAndRestrictSanctionedSellers} from "../utils/sanction.botUtils";
 
 export async function runSanctionBot(): Promise<void> {
   logger.info('Sanction Bot cron job started.');
-
   try {
     await findAndRestrictSanctionedSellers();
-    logger.info('SanctionBot job completed.');
   } catch (error) {
-    logger.error('Error in Sanction Bot cron job:', error);
+    logger.error('Error finding and restricting sanctioned sellers:', error);
+    throw error;
   }
 }
