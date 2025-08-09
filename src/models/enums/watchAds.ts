cf2500fba07d390c1fde1757a2c007333a355b72
Darin Hajou <DarinHajou@gmail.com>
2025-08-09 21:34:24 +0200

    Add WatchAdsSessionStatus enum

---
+++ src/models/enums/watchAds.ts
@@ -0,0 +1,10 @@
+// src/constants/watchAds.ts
+export const WATCH_ADS_SESSION_STATUS = {
+  Running: 'running',
+  Completed: 'completed',
+  Expired: 'expired',
+  Aborted: 'aborted',
+} as const;
+
+export type WatchAdsSessionStatus =
+  typeof WATCH_ADS_SESSION_STATUS[keyof typeof WATCH_ADS_SESSION_STATUS];
