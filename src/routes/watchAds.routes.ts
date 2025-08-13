af49d75bd82bdd6d1a80a3807bc9a52ae1428b1f
Darin Hajou <DarinHajou@gmail.com>
2025-08-13 21:48:47 +0200

    Add verifyToken middleware to ad watch route

--- src/routes/watchAds.routes.ts
+++ src/routes/watchAds.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { startWatchAdsSession } from '../controllers/watchAdsSessionController';
+import { verifyToken } from '../middlewares/verifyToken';
 
 const router = Router();
 
 // start/resume endpoint
-router.post('/watch-ads/session', startWatchAdsSession);
+router.post('/watch-ads/session', verifyToken, startWatchAdsSession);
 
 export default router;
